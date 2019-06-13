d816fd23f03c65328ee2c7bcb970750d85c4292c
nevilezhouiwei <https://github.com/nevilezhouiwei>
2019-06-13 22:17:59 +0800

    整理项目结构

--- 01RBAC/03code/rbacUI/js/portal/index.js
+++ 01RBAC/03code/rbacUI/js/portal/index.js
@@ -1,12 +1,13 @@
-$(function () {
-    //定义本地和远程加载数据模式
-   
+/**
+ * 视图框架
+ */
+$(function () {   
 
-    //accordion 中无法直接使用 URL 属性，所以直接的使用 ajax get方法取得数据。
+    
     $.ajax({
         type: 'get',
         dataType: "json",
-        url:  $.jpa.adaptURL({localURL:'../../js/portal/menu.json'}),
+        url:  $.jpa.adaptURL({localURL:'../../js/portal/menu.json'}),//accordion 中无法直接使用 URL 属性，所以直接的使用 ajax get方法取得数据。
         success: function (data) {
             $.each(data, function (i, n) {//加载父类节点即一级菜单
                 if (i == 0) {//显示第一个一级菜单下的二级菜单
@@ -32,7 +33,7 @@ $(function () {
     });
     //异步加载子节点，即二级菜单
     //在一级菜单中的 ul 中添加一个树。
-    //当从服务端请求数据时候可以优化暂时页面循环展开的问题。
+    //!!当从服务端请求数据时候可以优化暂时页面循环展开的问题。
     $('#layout_west_accordion').accordion({
         onSelect: function (title, index) {
             $("ul[name='" + title + "']").tree({
@@ -40,8 +41,8 @@ $(function () {
                 url:  $.jpa.adaptURL({localURL:'../../js/portal/menu2.json'}),
                 onClick: function (node) {
                     var text = node.text;
-                    //var url = node.attributes.url;
-                    var url = '../appmodules/demo/index.html';
+                    //var url = node.attributes.url;//可以填写进入动态的url
+                    var url = '../appmodules/demo/index.html';//表单demo
                     if ($('#tabs').tabs('exists', text)) {
                         $('#tabs').tabs('select', text); 
                     } else {
